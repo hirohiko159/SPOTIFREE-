7139272f2c10ab23a02942ea057aa312b2836e5a
hirohiko159 <106160803+hirohiko159@users.noreply.github.com>
2022-06-16 14:48:20 +0200

    Update client.py

--- client.py
+++ client.py
@@ -4,25 +4,15 @@ from re import A
 from sys import version
 import random
 import os
-import bdd
+# import bdd
 
-ip_address = "192.168.0.1"
-username = "daxtors"
-password = "loul"
+import paramiko
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect('127.0.0.1', username='daxtors', password='loul')
+reponse=''
 
-def connexion (user):
-    
-def creation_compte (users):
-    
-def search_key_word (music):
-    
-def show_playlist (playlist):
 
-def show_friends (friends):   
-
-def telechargement (doawnload):
-    
-def add_playlist (play):
     
      
 
@@ -30,12 +20,20 @@ while True:
     print ("############# Bienvenue sur SPOTIFREE #############")
     print ("\n"*5)
     print ("Connexion: (1) pour rentrer vos log, (2) pour creer un compte ")
-    if reponse not in ['1','2'] : 
+    if reponse not in ['1','2']: 
         reponse = input ("que voulez vous faire (1,2) ")
     if reponse in ['1']:
-        print("entre votre nom d'utilisateur et votre mot de passe")
-        connexion
-        if connexion == True:
+        userid=input("Username")
+        userpasswd=input("Password")
+        commande= 'python3 corescript.py authentification %s %s'%(userid,userpasswd)  
+        stdin, stdout, stderr = client.exec_command('python3 corescript.py authentification username password ')
+        print (stdout.read())
+        stdin.close()
+        # ssh spotifree@localhost 
+        # python3 corescript.py authentification username password
+        # response=..
+        stdout.read
+        if stdout==0:
             print ("########### Bievenue dans votre espace personelle ########")
             print ("(1)Chercher une musique")
             print ("(2)Playlist")
@@ -43,30 +41,32 @@ while True:
             if reponse in ['1']:
                 print ("\n"*5)
                 print ("veuillez entre un mot clef pour la chanson desirer:")
-                search_key_word
+                
                 print (" que voulez vous faire: a) telecharger la musique vers votre repertoire personnelle")
                 print ("b) ajouter a une playlist")
                 if reponse in ['a']:
-                    telechargement
+                    
                 if reponse in ['b']:
-                    add_playlist
+                    
             if reponse in ['2']:
                 print ("\n"*5)
-                show_playlist
+                
                 #ajoute supression playlist
                 #lecture d"une playlist (option aleatoire)
             if reponse in ['3']:
                 print ("\n"*5)
-                show_friends
+                
                 #ajouter ou suprrimer amis
                 #envoyer des messages
                 #partage playlist
+    else : 
+        print("mauvais login")
     if reponse in ['2']:
         print ("veuillez saisir un nom d'utilisateur et un mot de passe")
-        creation_compte
+        # creation_compte
         
-    
+client.close()    
         
-    
+
     
 
