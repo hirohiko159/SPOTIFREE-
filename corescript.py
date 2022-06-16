9b6a0236a0300fca2bedaaa70560656aebc6944d
tlevolle <24293650+tlevolle@users.noreply.github.com>
2022-06-16 18:20:19 +0200

    Update corescript.py
    ajouterMusiqueAPlaylist ne fonctionne pas, mais il fait au moins 70° dans ma pièce, donc j'abandonne.

--- corescript.py
+++ corescript.py
@@ -1,54 +1,53 @@
 import sys
+from unittest import result
 from bdd import Bdd
 
-def authentification(username, password):
-    bdd = Bdd()
+def authentification(bdd, username, password):
     userAuthentifie = bdd.verificationUtilisateur(username, password)
-    bdd.closeBdd()
     return userAuthentifie
 
-def rechercheMusique(mot_clef):
-    bdd = Bdd()
+def rechercheMusique(bdd, mot_clef):
     resultatRecherche = bdd.resultatRechercheMusiques(mot_clef)
-    bdd.closeBdd()
     return resultatRecherche
 
-def recherchePlaylists(nom_user):
-    bdd = Bdd()
+def recherchePlaylists(bdd, nom_user):
     resultatRecherche = bdd.resultatListePlaylists(nom_user)
-    bdd.closeBdd()
-    return resultatRecherche.split("|")[0]
+    split = resultatRecherche.split("\n")
+    liste_playlist=[]
+    for i in range(0,len(split)-1):
+        liste_playlist.append(split[i].split("|")[0])
+    return liste_playlist
 
-def recupererMusiquesDepuisPlaylist(nom_user):
-    bdd = Bdd()
+def recupererMusiquesDepuisPlaylist(bdd, nom_user):
     resultatRecherche = bdd.recupereMusiquesDepuisPlaylist(nom_user)
-    bdd.closeBdd()
     return resultatRecherche
 
-def ajouterMusiqueAPlaylist(nom_user, nom_musique, nom_playlist):
-    bdd = Bdd()
+def ajouterMusiqueAPlaylist(bdd, nom_user, nom_playlist, nom_musique):
     id_musique=bdd.recupererIdMusiqueDepuisNom(nom_musique)
     id_user=bdd.recupererIdUserDepuisNom(nom_user)
     bdd.ajouterMusiqueAPlaylist(nom_playlist,id_user,id_musique)
-    bdd.closeBdd()
     
 def main():
+    bdd = Bdd()
     if sys.argv[1] == "authentification":
         if sys.argv[2] != "" and sys.argv[3] != "":
-            # return authentification(sys.argv[2], sys.argv[3])
-            print("0")
-            return True
+            if authentification(bdd,sys.argv[2], sys.argv[3]):
+                print("0")
+            else:
+                print("1")
     elif sys.argv[1] == "recherche_musique":
         if sys.argv[2] != "":
-            print (rechercheMusique(sys.argv[2]))
+            print (rechercheMusique(bdd, sys.argv[2]))
     elif sys.argv[1] == "telechargement":
         #TODO
-        return "Erreur"
+        print("Erreur")
     elif sys.argv[1] == "recherche_liste_playlists":
         if sys.argv[2] != "":
-            return recherchePlaylists(sys.argv[2])
+            print(*recherchePlaylists(bdd, sys.argv[2]), sep="|")
     elif sys.argv[1] == "ajouter_musique_a_playlist":
         if sys.argv[2] != "" and sys.argv[3] != "" and sys.argv[4] != "":
-            return ajouterMusiqueAPlaylist(sys.argv[2], sys.argv[4], sys.argv[3])
+            ajouterMusiqueAPlaylist(bdd, sys.argv[2], sys.argv[3], sys.argv[4])
+    bdd.closeBdd()
+
 
 main()
