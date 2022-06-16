2c78985f65fad938361c677af11040707b85f187
hirohiko159 <106160803+hirohiko159@users.noreply.github.com>
2022-06-16 17:26:51 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,6 @@
+
+
+    
 #!/usr/bin/env python
 from re import A
 import paramiko
@@ -8,60 +11,70 @@ import os
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect('127.0.0.1', username='daxtors', password='loul')
+client.connect('127.0.0.1', username='spotifree', password='loul')
 reponse=''
+login=False
 
-while True:
-    print ("############# Bienvenue sur SPOTIFREE #############")
+print ("############# Bienvenue sur SPOTIFREE #############")
+while login==False:
+   
     print ("\n"*5)
     print ("Connexion: (1) pour rentrer vos log, (2) pour creer un compte ")
     if reponse not in ['1','2']: 
         reponse = input ("que voulez vous faire (1,2) ")
     if reponse in ['1']:
-        userid=input("Username")
-        userpasswd=input("Password")
+        userid=input("Username: ")
+        userpasswd=input("Password: ")
         commande= 'python3 corescript.py authentification %s %s'%(userid,userpasswd)  
         stdin, stdout, stderr = client.exec_command(commande)
-        print(stdout.read())
+        print(stderr.read())
+        rep=(stdout.read()).decode().split('\n')[0] 
+        print(rep)
         stdin.close()
-
-        if stdout==0:
-            print ("########### Bievenue dans votre espace personel ########")
-            print ("(1)Chercher une musique")
-            print ("(2)Playlist")
-            print ("(3)Spotifriends")
-
-            if reponse in ['1']:
-                print ("\n"*5)
-                print ("veuillez entre un mot clef pour la chanson desirer:")
-                
-                print (" que voulez vous faire: a) telecharger la musique vers votre repertoire personnelle")
-                print ("b) ajouter a une playlist")
-                if reponse in ['a']:
-                    print()
-                    
-                if reponse in ['b']:
-                    print()
-                    
-            if reponse in ['2']:
-                print ("\n"*5)
-                
-                #ajoute supression playlist
-                #lecture d"une playlist (option aleatoire)
-            if reponse in ['3']:
-                print ("\n"*5)
-                
-                #ajouter ou suprrimer amis
-                #envoyer des messages
-                #partage playlist
-    else : 
-        print("mauvais login")
+        if rep=='0':
+            login=True   
+        else : 
+            print("mauvais login")
     if reponse in ['2']:
         print ("veuillez saisir un nom d'utilisateur et un mot de passe")
         #TODO + corescruipt fonction création utilisateur
+print ("########### Bienvenue dans votre espace personel ########")
+while True:
+    print ("(1)Chercher une musique")
+    print ("(2)Playlist")
+    print ("(3)Spotifriends")
+    reponse=input("que voulez vous faire?: ")
+    if reponse in ['1']:
+        print ("\n"*5)
+        mot=input ("veuillez entre un mot clef pour la chanson desirer:")
+        commande= 'python3 corescript.py recherche_musique %s '%(mot)  
+        stdin, stdout, stderr = client.exec_command(commande)
+        rep=(stdout.read()).decode().split('\n')[0] 
+        print(rep)
+        # print (" que voulez vous faire: a) telecharger la musique vers votre repertoire personnelle")
+        # print ("b) ajouter a une playlist")
+        # if reponse in ['a']:
+        #     print()
+            
+        # if reponse in ['b']:
+        #     print()
+            
+    if reponse in ['2']:
+        print ("\n"*5)
+        
+        #ajoute supression playlist
+        #lecture d"une playlist (option aleatoire)
+    if reponse in ['3']:
+        print ("\n"*5)
+        
+        #ajouter ou suprrimer amis
+        #envoyer des messages
+        #partage playlist
+
         
 client.close()    
         
 
     
+
 
